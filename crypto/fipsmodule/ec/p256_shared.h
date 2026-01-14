a875301c75ad25ce60daf0b3788c0e44b67cc0f6
AndyTitu <andi.titu@agilebits.com>
2026-01-14 20:02:44 +0200

    Backport gfp

--- crypto/fipsmodule/ec/p256_shared.h
+++ crypto/fipsmodule/ec/p256_shared.h
@@ -1,20 +1,25 @@
-/*
- * Copyright 2014-2016 The OpenSSL Project Authors. All Rights Reserved.
- * Copyright (c) 2014, Intel Corporation. All Rights Reserved.
- *
- * Licensed under the OpenSSL license (the "License").  You may not use
- * this file except in compliance with the License.  You can obtain a copy
- * in the file LICENSE in the source distribution or at
- * https://www.openssl.org/source/license.html
- *
- * Originally written by Shay Gueron (1, 2), and Vlad Krasnov (1)
- * (1) Intel Corporation, Israel Development Center, Haifa, Israel
- * (2) University of Haifa, Israel
- *
- * Reference:
- * S.Gueron and V.Krasnov, "Fast Prime Field Elliptic Curve Cryptography with
- *                          256 Bit Primes"
- */
+// Copyright 2014-2016 The OpenSSL Project Authors. All Rights Reserved.
+// Copyright (c) 2014, Intel Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Originally written by Shay Gueron (1, 2), and Vlad Krasnov (1)
+// (1) Intel Corporation, Israel Development Center, Haifa, Israel
+// (2) University of Haifa, Israel
+//
+// Reference:
+// S.Gueron and V.Krasnov, "Fast Prime Field Elliptic Curve Cryptography with
+//                          256 Bit Primes"
 
 #ifndef OPENSSL_HEADER_EC_P256_SHARED_H
 #define OPENSSL_HEADER_EC_P256_SHARED_H
@@ -23,7 +28,8 @@
 
 #include "../bn/internal.h"
 
-#if !defined(OPENSSL_NO_ASM) && defined(OPENSSL_X86_64) && \
+#if !defined(OPENSSL_NO_ASM) && \
+    (defined(OPENSSL_X86_64) || defined(OPENSSL_AARCH64)) && \
     !defined(OPENSSL_SMALL)
 # define OPENSSL_USE_NISTZ256
 #endif
@@ -39,7 +45,6 @@
 #define P256_LIMBS (256 / BN_BITS2)
 
 // A P256_POINT represents a P-256 point in Jacobian coordinates.
-// All coordinates are in the Montgomery domain.
 typedef struct {
   BN_ULONG X[P256_LIMBS];
   BN_ULONG Y[P256_LIMBS];
@@ -54,4 +59,4 @@ static inline void p256_scalar_bytes_from_limbs(
   bytes_out[32] = 0;
 }
 
-#endif
+#endif /* !defined(OPENSSL_USE_NISTZ256) */
